4f79bd8f27b40f4a58ab49ca33faaf83e86c207d
Shaurya Singh <38398743+IG-11-Kuiil@users.noreply.github.com>
2025-08-23 12:41:10 +0530

    Create cell1.py

---
+++ cell1.py
@@ -0,0 +1,4 @@
+# Cell 1: installs
+!pip -q install --upgrade pip
+!pip -q install --no-cache-dir "transformers==4.43.3" "accelerate>=0.33" sentencepiece
+!pip -q install --no-cache-dir git+https://github.com/VarunGumma/IndicTransToolkit.git
